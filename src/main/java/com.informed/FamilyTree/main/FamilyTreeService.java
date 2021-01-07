7fd36a5cde33ee30c2198115152540da548e07ca
leusoijames <leusoijames@gmail.com>
2021-01-07 16:09:48 +0000

    Person and place

--- src/main/java/com.informed/FamilyTree/main/FamilyTreeService.java
+++ src/main/java/com.informed/FamilyTree/main/FamilyTreeService.java
@@ -1,5 +1,6 @@
 package com.informed.FamilyTree.main;
 
+import com.informed.FamilyTree.controller.FamilyTreeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,14 +13,15 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.informed.FamilyTree"})
-@EntityScan("com.informed.FamilyTree")
+@EntityScan("com.informed.FamilyTree.domain")
 @EnableJpaRepositories("com.informed.FamilyTree")
 
 public class FamilyTreeService {
     public static void main(String[] args) throws SQLException {
         System.out.println("Starting FamilyTree setup");
         ConfigurableApplicationContext context = SpringApplication.run(FamilyTreeService.class, args);
-        System.out.println("Setup	finished");
+//        FamilyTreeController familyTreeController = context.getBean(FamilyTreeController.class);
+        System.out.println("Setup finished");
 
     }
 
