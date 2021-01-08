e907436a56204cc7fa8611c5a07e9db5b7e08ef7
AustenCWells <a_wells@btinternet.com>
2021-01-08 11:22:46 +0000

    Added Javascript

--- src/main/resources/public/FamilyTree.js
+++ src/main/resources/public/FamilyTree.js
@@ -1,4 +1,14 @@
-console.log('Starting the bookshop');
+console.log('Starting the Family Tree Service');
+
+function dateToYMD(dateString) {
+    let date = new Date (dateString);
+    var d = date.getDate();
+    var m = date.getMonth() + 1; //Month from 0 to 11
+    var y = date.getFullYear();
+    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+}
+
+//console.log(dateToYMD(new Date(2017,10,5))); // Nov 5
 
 $(document).ready(
     function () {
@@ -12,20 +22,23 @@ $(document).ready(
                    .show('slow');
         });
         //Set up some show list of books behaviour
-        $("#showBooks").click(
+        $("#showPerson").click(
             function () {
-                console.log('running click on showBooks');
+                console.log('running click on showAllPersons');
                 event.preventDefault();
-                $.get("http://localhost:9696/FamilyTree/list",
-                      function(books) {
-                        console.log(books);
-                        $.each(books, function(i, book) {
-                        console.log(i, ', '. book);
+                $.get("http://localhost:8080/FamilyTree/list",
+                      function(persons) {
+                        console.log(persons);
+                        $.each(persons, function(i, person) {
+                        console.log(i, ', '. person);
                         let html = "<li>";
-                        html += book.title + " by " + book.author;
-                        html += ", " + book.isbn + ", " + book.category;
+                        html += person.personName + " ID is " + person.personID+ ".";
+                        html += "&nbsp" + person.personName + " was born on " + dateToYMD(person.dob) + ". " + person.personName + " Birth Place ID is " + person.birthPlaceId + ". ";
+                        html += person.personName + " has " + person.hasChildren + " children. " + person.personName + " has been married " + person.married + " before " + "." + person.personName + " died on " + dateToYMD(person.deathDate)+ ". ";
+                        html += person.personName + " Death Place ID is " + person.deathPlaceId + ".";
                         html += "</li>";
-                        $('#booklist').append(html);
+                        $('#AllPersonList').append(html);
                         });
                       });
-            })
+            });
+        });
